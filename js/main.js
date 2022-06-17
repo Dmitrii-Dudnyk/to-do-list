b4dd1607af49341c50eb5113751bb10cc2b9acb4
Dmitrii Dudnyk <dvdudnyk@gmail.com>
2022-06-17 21:15:06 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,86 +1,102 @@
 const input = document.querySelector(".input-task");
-const addTaskBtn = document.querySelector(".add-task");
 const tasksList = document.querySelector(".tasks-list");
+const inputBoxEl = document.querySelector(".input-box");
+
+const modal = document.getElementById("modal-box");
+const modalCancelBtn = document.querySelector(".modal-cancel");
+const modalDeleteBtn = document.querySelector(".modal-delete");
 
 let tasks;
+let index;
 
-!localStorage.tasks ? (tasks = []) : (tasks = JSON.parse(localStorage.getItem("tasks")));
+localStorage.tasks ? (tasks = JSON.parse(localStorage.getItem("tasks"))) : (tasks = []);
 
 function Task(description) {
   this.description = description;
   this.completed = false;
 }
 
-function createListMarkup(task, index) {
+function updateLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+addListMarkup();
+
+function addListMarkup() {
+  tasksList.innerHTML = "";
+  if (tasks.length > 0) {
+    tasks.forEach(task => {
+      tasksList.innerHTML += createListMarkup(task);
+    });
+  }
+}
+
+function createListMarkup(task) {
   return `
     <li class="tasks-list__item ${task.completed ? "checked" : ""}">
       <span class="tasks-list__text ${task.completed ? "checked" : ""}">${task.description}</span>
       <div class="btn-box">
         <input class="complete-btn" type="checkbox" ${task.completed ? "checked" : ""}>
         <label for="complete-btn"></label>
-        <span class="material-icons remove-btn">delete_forever</span>
+        <span class="remove-btn material-icons">delete_forever</span>
       </div>
     </li>
   `;
 }
 
-addListMarkup();
+inputBoxEl.addEventListener("click", e => {
+  if (e.target.className == "add-task" && input.value != "") renderMarkup();
+});
 
-addTaskBtn.addEventListener("click", () => {
-  if (input.value != "") {
-    tasks.push(new Task(input.value));
+inputBoxEl.addEventListener("keydown", e => {
+  if (e.keyCode === 13 && input.value != "") renderMarkup();
+});
+
+function renderMarkup() {
+  tasks.push(new Task(input.value));
+  updateLocalStorage();
+  addListMarkup();
+  input.value = "";
+}
+
+tasksList.addEventListener("click", e => {
+  if (e.target.className == "complete-btn") completeTask(e);
+  if (e.target.className == "remove-btn material-icons") openModal(e);
+});
+
+modal.addEventListener("click", e => {
+  if (e.target.className == "modal-close" || e.target.className == "modal-cancel") modal.style.display = "none";
+  if (e.target.className == "modal-delete") {
+    tasks.splice(index, 1);
     updateLocalStorage();
     addListMarkup();
-    input.value = "";
+    modal.style.display = "none";
   }
 });
 
-input.addEventListener("keydown", e => {
-  if (e.keyCode === 13 && input.value != "") {
-    tasks.push(new Task(input.value));
-    updateLocalStorage();
-    addListMarkup();
-    input.value = "";
-  }
-});
-
-function updateLocalStorage() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-function addListMarkup() {
-  tasksList.innerHTML = "";
-  if (tasks.length > 0) {
-    tasks.forEach((task, index) => {
-      tasksList.innerHTML += createListMarkup(task, index);
-    });
-  }
-  addEventClickBtns();
-}
-
-function addEventClickBtns() {
+function completeTask(e) {
   const tasksListItem = document.querySelectorAll(".tasks-list__item");
   const tasksListText = document.querySelectorAll(".tasks-list__text");
   const completeBtns = document.querySelectorAll(".complete-btn");
-  const removeBtn = document.querySelectorAll(".remove-btn");
   for (let i = 0; i < completeBtns.length; i += 1) {
-    completeBtns[i].addEventListener("click", () => {
+    if (e.target == completeBtns[i]) {
       tasks[i].completed = !tasks[i].completed;
       updateLocalStorage();
-      if (tasks[i].completed) {
-        tasksListItem[i].classList.add("checked");
-        tasksListText[i].classList.add("checked");
-      } else {
-        tasksListItem[i].classList.remove("checked");
-        tasksListText[i].classList.remove("checked");
-      }
-    });
-    removeBtn[i].addEventListener("click", () => {
-      if (confirm("You really want to delete the task?")) {
-        tasks.splice(i, 1);
-        updateLocalStorage();
-        addListMarkup();
-      }
-    });
+    }
+    if (tasks[i].completed) {
+      tasksListItem[i].classList.add("checked");
+      tasksListText[i].classList.add("checked");
+    } else {
+      tasksListItem[i].classList.remove("checked");
+      tasksListText[i].classList.remove("checked");
+    }
+  }
+}
+
+function openModal(e) {
+  const removeBtns = Array.from(document.querySelectorAll(".remove-btn.material-icons"));
+  index = removeBtns.findIndex(item => item === e.target);
+  if (index != -1) {
+    modal.style.display = "block";
   }
 }
